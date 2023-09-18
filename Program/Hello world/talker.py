c93d2572cda134822e7a2b95d659b94eeb2cb7f3
Kevin Alberto <132956095+KevinAlberto01@users.noreply.github.com>
2023-09-18 16:25:08 -0600

    Update talker.py

--- Program/Hello world/talker.py
+++ Program/Hello world/talker.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python
 
+###############################    Setup environment and import libraries 
+import rospy
+from std_msgs.msg import String
+###############################
+
+########################################################### Create variables and setup node 
+if __name__ == '__main__':
+    pub = rospy.Publisher("chatter", String, queue_size=10)
+    rospy.init_node("talker")
+    rate = rospy.Rate(10)
+###########################################################
+
+########################################################### Publish message
+    while not rospy.is_shutdown():
+        hello_str = "helllo world %s" % rospy.get_time()
+        pub.publish(hello_str)
+
+############################################################ Wait to run again         
+        rate.sleep()
+############################################################
